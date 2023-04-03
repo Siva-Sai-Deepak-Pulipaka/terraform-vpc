ce04ff4cba9132041a83507d8c44efbbae34d656
Siva-Sai-Deepak-Pulipaka <18l11a0518@gmail.com>
2023-04-04 02:04:58 +0530

    created internet & nat gw, eip for public subnets

--- main.tf
+++ main.tf
@@ -9,5 +9,32 @@ resource "aws_vpc" "main" {
 }
 # we are using merge function to combine tags in vars.tf and tags in terraform.tfvars 
 
+# internet gateway
+# As we already know that internet gateway is for public subnets only
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
 
+  tags = merge(
+    var.tags,
+    { Name = "${var.env}-igw" }
+  )
+}
+# Elastic ip creation for NAT purpose
+resource "aws_eip" "nat" {
+  for_each = var.public_subnets
+  vpc      = true
+}
+# NAT gateway
+resource "aws_nat_gateway" "nat-gateways" {
+  for_each      = var.public_subnets
+  allocation_id = aws_eip.nat[each.value["name"]].id
+  subnet_id     = aws_subnet.public_subnets[each.value["name"]].id
+
+  tags = merge(
+    var.tags,
+    { Name = "${var.env}-${each.value["name"]}" }
+  )
+
+
+}
 
--- routetable.tf
+++ routetable.tf
@@ -2,6 +2,11 @@
 resource "aws_route_table" "public-route-table" {
   vpc_id = aws_vpc.main.id
 
+   route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id       #internet gateway is present in main.tf
+  }
+
   for_each          = var.public_subnets
   tags = merge(
     var.tags,
