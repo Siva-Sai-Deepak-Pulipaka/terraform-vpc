879dd173452e31a72fa7b815997ee3f1c54cbb1d
Siva-Sai-Deepak-Pulipaka <18l11a0518@gmail.com>
2023-04-04 01:15:08 +0530

    changes in vpc module

--- main.tf
+++ main.tf
@@ -8,14 +8,14 @@ resource "aws_vpc" "main" {
     )
 }
 # we are using merge function to combine tags in vars.tf and tags in terraform.tfvars 
-# public route table
+
 
 # public subnets
 resource "aws_subnet" "public_subnets" {
-  vpc_id = aws_vpc.main.id
+  vpc_id            = aws_vpc.main.id
 
-  for_each = var.public_subnets
-  cidr_block = each.value["cidr_block"]
+  for_each          = var.public_subnets
+  cidr_block        = each.value["cidr_block"]
   availability_zone = each.value["availability_zone"]
   tags = merge(
     var.tags,
@@ -28,8 +28,8 @@ resource "aws_subnet" "public_subnets" {
 resource "aws_subnet" "private_subnets" {
   vpc_id = aws_vpc.main.id
 
-  for_each = var.private_subnets
-  cidr_block = each.value["cidr_block"]
+  for_each          = var.private_subnets
+  cidr_block        = each.value["cidr_block"]
   availability_zone = each.value["availability_zone"]
   tags = merge(
     var.tags,
@@ -37,10 +37,11 @@ resource "aws_subnet" "private_subnets" {
   )
   
 }
+# public route table
 resource "aws_route_table" "public-route-table" {
   vpc_id = aws_vpc.main.id
 
-  for_each = var.public_subnets
+  for_each          = var.public_subnets
   tags = merge(
     var.tags,
     { Name = "${var.env}-${each.value["name"]}" }
@@ -49,7 +50,7 @@ resource "aws_route_table" "public-route-table" {
 
 # Public route table association
 resource "aws_route_table_association" "public-association" {
-  for_each = var.public_subnets
+  for_each       = var.public_subnets
   subnet_id      = aws_subnet.public_subnets[each.value["name"]].id
 # subnet_id      = lookup(lookup(aws_subnet.public_subnets, each.value["name"], null), "id", null)     #this is also we can use to get subnet_id
   route_table_id = aws_route_table.public-route-table[each.value["name"]].id
@@ -57,9 +58,9 @@ resource "aws_route_table_association" "public-association" {
 
 # private route table
 resource "aws_route_table" "private-route-table" {
-  vpc_id = aws_vpc.main.id
+  vpc_id        = aws_vpc.main.id
 
-  for_each = var.private_subnets
+  for_each      = var.private_subnets
   tags = merge(
     var.tags,
     { Name = "${var.env}-${each.value["name"]}" }
@@ -68,8 +69,8 @@ resource "aws_route_table" "private-route-table" {
 
 # Private route table association
 resource "aws_route_table_association" "private-association" {
-  for_each = var.private_subnets
-  subnet_id      = aws_subnet.private_subnets[each.value["name"]].id
-# subnet_id      = lookup(lookup(aws_subnet.private_subnets, each.value["name"], null), "id", null)     #this is also we can use to get subnet_id
+  for_each       = var.private_subnets
+  # subnet_id    = aws_subnet.private_subnets[each.value["name"]].id
+ subnet_id       = lookup(lookup(aws_subnet.private_subnets, each.value["name"], null), "id", null)     #this is also we can use to get subnet_id
   route_table_id = aws_route_table.private-route-table[each.value["name"]].id
 }
